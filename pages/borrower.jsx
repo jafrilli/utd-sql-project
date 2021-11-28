d374336feb7f49577a0ad09b2194e698c4b7e06f
Mrunmayi Sathaye <mrunmayisathaye@Mrunmayis-MacBook-Pro.local>
2021-11-28 12:45:16 -0600

    added cardid to profile

--- pages/borrower.jsx
+++ pages/borrower.jsx
@@ -66,7 +66,7 @@ export default function Borrower() {
                     </div>
                     <div className="text-center">
                         <p className="font-bold text-3xl">{data[0].name}</p>
-                        <p className="text-md text-library-text text-opacity-70 mb-3">Member since 2021</p>
+                        <p className="text-md text-library-text text-opacity-70 mb-3">{cardId}</p>
                         <LibraryButton onClick={() => { setCardId(null); router.push('/login') }} text="Sign Out">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M9 3a1 1 0 012 0v5.5a.5.5 0 001 0V4a1 1 0 112 0v4.5a.5.5 0 001 0V6a1 1 0 112 0v5a7 7 0 11-14 0V9a1 1 0 012 0v2.5a.5.5 0 001 0V4a1 1 0 012 0v4.5a.5.5 0 001 0V3z" clipRule="evenodd" />
